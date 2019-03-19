237c3f1fa2a0e1a89d6d3f3873b46e1408bdd122
Djordje Jovanovic <47816396+djordjejovanovic@users.noreply.github.com>
2019-03-19 22:14:01 +0100

    Add files via upload

---
+++ Glavni.py
@@ -0,0 +1,27 @@
+def unos_za_ponedeljak():
+    print("Unesite raspored za ponedeljak:")
+    radi=True;
+    ponedeljak_lista=[]
+    while radi:
+        predmet = input("Unesite ime predmeta: ")
+        vreme = input ("Unesite vreme predmeta (u formatu hh:mm - hh:mm): ")
+        ponedeljak_lista.append(predmet)
+        ponedeljak_lista.append(vreme)
+
+        odgovor=input("Da li zelite da unesete jos jedan predmet? (Odgovoriti sa Da/Ne)")
+        if odgovor == 'Da':
+            radi=True
+        else:
+            radi=False
+
+    return ponedeljak_lista
+
+lista_ponedeljak=unos_za_ponedeljak()
+
+print("Ponedeljak:")
+i=len(lista_ponedeljak)
+j=0
+
+while j<i:
+    print(lista_ponedeljak[j] + " " + lista_ponedeljak[j+1])
+    j+=2

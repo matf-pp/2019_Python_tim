5be592b6d878b0408134f3464d71afe546695c09
viseslavdj <48122540+viseslavdj@users.noreply.github.com>
2019-05-08 14:48:18 +0200

    Update raspored_update.py

--- raspored_update.py
+++ raspored_update.py
@@ -1,21 +1,13 @@
 import tkinter
 from tkinter import *
+import random
 
+colors = ["red", "orange", "yellow", "green", "blue", "violet", "silver", "lightblue", "white", "green"]
 
-def provera_zauzeto(dan, vreme, trajanje):
-    # za dan nam prosledjuje i
-
-    indikator = True
-    i = dan
-    j = vreme - 7
-
-
-#  while trajanje > 0 :
-# if  labela empty then return false, else true
-
-# return True
 
 def upisipredmet():
+    dodaj.configure(bg=random.choice(colors))
+    obrisi.configure(bg="#ffffff")
     predmet = predmet1.get()
     predmet1.delete(0, 30)
     danunedelji = dan1.get()
@@ -39,19 +31,16 @@ def upisipredmet():
 
     j = vremepocetkacasa - 7
 
-    # if provera_zauzeto(i, vremepocetkacasa, vremetrajanjacasa) != True:
-    # break
-    # l = Label(text="Ovaj termin je zauzet", relief=RIDGE)
-    # l.grid(row=2, column=5, sticky=NSEW)
-
     while vremetrajanjacasa > 0:
-        l = Label(text=predmet, relief=RIDGE)
+        l = Label(text=predmet, relief=RIDGE, bg ="lightblue")
         l.grid(row=i, column=j, sticky=NSEW)
 
         vremetrajanjacasa = vremetrajanjacasa - 1
         j = j + 1
 
 def obrisipredmet():
+    obrisi.configure(bg=random.choice(colors))
+    dodaj.configure(bg="#ffffff")
     predmet = predmet1.get()
     predmet1.delete(0, 30)
     danunedelji = dan1.get()
@@ -76,7 +65,7 @@ def obrisipredmet():
     j = vremepocetkacasa - 7
 
     while vremetrajanjacasa > 0:
-        l = Label(text=" ", relief=RIDGE)
+        l = Label(text=" ", relief=RIDGE,  bg="#ffffff")
         l.grid(row=i, column=j, sticky=NSEW)
 
         vremetrajanjacasa = vremetrajanjacasa - 1
@@ -142,53 +131,54 @@ petak = Label(text="Petak ", bg="silver",
                   heigh=1, width=10, relief=RIDGE)
 petak.grid(column=0, row=10)
 
-label1 = Label(text="08:00 - 09:00", relief=RIDGE)
+label1 = Label(text="08:00 - 09:00", bg="lightgreen", relief=RIDGE)
 label1.grid(column=1, row=5)
 
-label2 = Label(text="09:00 - 10:00", relief=RIDGE)
+label2 = Label(text="09:00 - 10:00", bg="lightgreen", relief=RIDGE)
 label2.grid(column=2, row=5)
 
-label3 = Label(text="10:00 - 11:00", relief=RIDGE)
+label3 = Label(text="10:00 - 11:00", bg="lightgreen", relief=RIDGE)
 label3.grid(column=3, row=5)
 
-label4 = Label(text="11:00 - 12:00", relief=RIDGE)
+label4 = Label(text="11:00 - 12:00", bg="lightgreen", relief=RIDGE)
 label4.grid(column=4, row=5)
 
-label5 = Label(text="12:00 - 13:00", relief=RIDGE)
+label5 = Label(text="12:00 - 13:00", bg="lightgreen", relief=RIDGE)
 label5.grid(column=5, row=5)
 
-label6 = Label(text="13:00 - 14:00", relief=RIDGE)
+label6 = Label(text="13:00 - 14:00", bg="lightgreen", relief=RIDGE)
 label6.grid(column=6, row=5)
 
-label7 = Label(text="14:00 - 15:00", relief=RIDGE)
+label7 = Label(text="14:00 - 15:00", bg="lightgreen", relief=RIDGE)
 label7.grid(column=7, row=5)
 
-label8 = Label(text="15:00 - 16:00", relief=RIDGE)
+label8 = Label(text="15:00 - 16:00", bg="lightgreen", relief=RIDGE)
 label8.grid(column=8, row=5)
 
-label9 = Label(text="16:00 - 17:00", relief=RIDGE)
+label9 = Label(text="16:00 - 17:00", bg="lightgreen", relief=RIDGE)
 label9.grid(column=9, row=5)
 
-label10 = Label(text="17:00 - 18:00", relief=RIDGE)
+label10 = Label(text="17:00 - 18:00", bg="lightgreen", relief=RIDGE)
 label10.grid(column=10, row=5)
 
-label11 = Label(text="18:00 - 19:00", relief=RIDGE)
+label11 = Label(text="18:00 - 19:00", bg="lightgreen", relief=RIDGE)
 label11.grid(column=11, row=5)
 
-label12 = Label(text="19:00 - 20:00", relief=RIDGE)
+label12 = Label(text="19:00 - 20:00", bg="lightgreen", relief=RIDGE)
 label12.grid(column=12, row=5)
 
-label13 = Label(text="20:00 - 21:00", relief=RIDGE)
+label13 = Label(text="20:00 - 21:00", bg="lightgreen", relief=RIDGE)
 label13.grid(column=13, row=5)
 
+
 dodaj = Button(window, text="Upisi", command=upisipredmet)
-dodaj.grid(column=3, row = 2, sticky = NSEW)
+dodaj.grid(column=4, row = 2, sticky = NSEW)
 
 obrisi = Button(window, text="Obrisi", command=obrisipredmet)
-obrisi.grid(column=4,row=2, sticky = NSEW)
+obrisi.grid(column=5,row=2, sticky = NSEW)
 
-# primer
-l = Label(text="Proba", relief=RIDGE)
-l.grid(row=9, column=5, sticky=NSEW)
+ukloni = Button(window, text="Ukloni", command = ukloni)
+ukloni.grid(column=6, row =2 , sticky=NSEW)
+
 
 window.mainloop()

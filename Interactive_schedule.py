1a8f8c7e30a12d3e01f6f80b3a6c6c24349af9d0
Djorjde <djordje.jovanovic.4896@gmail.com>
2019-05-24 00:45:22 +0200

    Nova izmena

--- Interactive_schedule.py
+++ Interactive_schedule.py
@@ -3,11 +3,47 @@ from tkinter import *
 import random
 from datetime import datetime
 import tkinter.messagebox
+import xlwt
+from xlwt import Workbook
 
 
 colors = ["PaleTurquoise4", "CadetBlue1", "medium spring green", "green yellow", "lime green",
           "violet", "silver", "lightblue", "snow", "pale violet red", "maroon"]
 dani = ["Ponedeljak", "Utorak", "Sreda", "Cetvrtak", "Petak", "Subota", "Nedelja"]
+
+
+def exportuj():
+    ime = ime_excel.get()
+    sheet1 = wb.add_sheet('Sheet 1')
+
+    sheet1.write(0, 0, "Dan\ vreme")
+    sheet1.write(0, 1, "08:00 - 09:00")
+    sheet1.write(0, 2, "09:00 - 10:00")
+    sheet1.write(0, 3, "10:00 - 11:00")
+    sheet1.write(0, 4, "11:00 - 12:00")
+    sheet1.write(0, 5, "12:00 - 13:00")
+    sheet1.write(0, 6, "13:00 - 14:00")
+    sheet1.write(0, 7, "14:00 - 15:00")
+    sheet1.write(0, 8, "15:00 - 16:00")
+    sheet1.write(0, 9, "16:00 - 17:00")
+    sheet1.write(0, 10, "17:00 - 18:00")
+    sheet1.write(0, 11, "18:00 - 19:00")
+    sheet1.write(0, 12, "19:00 - 20:00")
+    sheet1.write(0, 13, "20:00 - 21:00")
+
+    sheet1.write(1, 0, "Ponedeljak")
+    sheet1.write(2, 0, "Utorak")
+    sheet1.write(3, 0, "Sreda")
+    sheet1.write(4, 0, "Cetvrtak")
+    sheet1.write(5, 0, "Petak")
+    sheet1.write(6, 0, "Subota")
+    sheet1.write(7, 0, "Nedelja")
+
+    for i in range(0, 7):
+        for j in range(0, 14):
+            sheet1.write(i + 1, j + 1, matrica[i][j])
+
+    wb.save(ime + ".xls")
 
 
 def dodaj_vikend():
@@ -21,7 +57,8 @@ def dodaj_vikend():
 
 def ukloni_vikend():
     # for i in range(11, 13):
-    # for j in range(1, 14):
+    #    l[i]=[" "]
+
 
     subota.grid_remove()
     nedelja.grid_remove()
@@ -58,6 +95,7 @@ def upisipredmet():
     if vremepocetkacasa >= 8 and vremepocetkacasa <= 21 and vremepocetkacasa + duzinatrajanjacasa <= 21:
         while duzinatrajanjacasa > 0:
             if j <= 13:
+                matrica[i - 6][j - 1] = predmet
                 l = Label(text=predmet, relief=RIDGE, bg="lightblue").grid(row=i, column=j, sticky=NSEW)
                 duzinatrajanjacasa = duzinatrajanjacasa - 1
                 j = j + 1
@@ -65,6 +103,11 @@ def upisipredmet():
     else:
         tkinter.messagebox.showinfo("Upozorenje", "Greska!\nMolimo Vas da unesete tacno vreme i duzinu trajanja casa."
                                                   "\nCasovi pocinju u 8:00, a zavrsavaju se u 21:00.")
+
+    # for i in range(0, 7):
+    # for j in range(0, 14):
+    # print(matrica[i][j])
+    # print("\n")
 
 def obrisipredmet():
     obrisi.configure(bg=random.choice(colors))
@@ -100,12 +143,14 @@ def obrisipredmet():
         while duzinatrajanjacasa > 0:
             if j <= 13:
                 l = Label(text=" ", relief=RIDGE, bg="gray95").grid(row=i, column=j, sticky=NSEW)
+                matrica[i - 6][j - 1]=" "
                 duzinatrajanjacasa = duzinatrajanjacasa - 1
                 j = j + 1
 
     else:
         tkinter.messagebox.showinfo("Upozorenje", "Greska!\nMolimo Vas da unesete tacno vreme i duzinu trajanja casa."
                                                   "\nCasovi pocinju u 8:00, a zavrsavaju se u 21:00.")
+
 
 window = Tk()
 window.title("Interactive schedule")
@@ -131,6 +176,9 @@ vrt.grid(column=0, row=3)
 
 vrt1 = Entry(window, width=11)
 vrt1.grid(column=1, row=3)
+
+matrica = [[" "] * 14 for x in range(0, 7)]
+
 
 l = [[] * 14] * 7
 for i in range(6,11):
@@ -225,5 +273,13 @@ vikendp.grid(column=6, row=2, sticky=NSEW)
 
 vikendm = Button(window, text="Vikend -", command=ukloni_vikend)
 vikendm.grid(column=7, row=2, sticky=NSEW)
+
+wb = xlwt.Workbook()
+
+excel = Button(window, text="Save", command=exportuj)
+excel.grid(column=6, row=14, sticky=NSEW)
+
+ime_excel = Entry(window, width=11)
+ime_excel.grid(column=7, row=14)
 
 window.mainloop()

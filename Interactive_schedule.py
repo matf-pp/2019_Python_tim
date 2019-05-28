e2f0a6915daab9fc13e62eff8eceeed84793c63d
viseslavdj <48122540+viseslavdj@users.noreply.github.com>
2019-05-28 13:39:20 +0200

    Update Interactive_schedule.py

--- Interactive_schedule.py
+++ Interactive_schedule.py
@@ -3,13 +3,14 @@ from tkinter import *
 import random
 from datetime import datetime
 import tkinter.messagebox
-
-from xlwt import Workbook
 import xlwt
+from xlwt import Workbook
+
 
 colors = ["PaleTurquoise4", "CadetBlue1", "medium spring green", "green yellow", "lime green",
           "violet", "silver", "lightblue", "snow", "pale violet red", "maroon"]
 dani = ["Ponedeljak", "Utorak", "Sreda", "Cetvrtak", "Petak", "Subota", "Nedelja"]
+
 
 def exportuj():
     ime = ime_excel.get()
@@ -53,7 +54,6 @@ def exportuj():
 
 
 def proveri_zauzeto(i, j, k):
-
     while j <= k:
         if not matrica[i][j] == " ":
             return False
@@ -74,6 +74,8 @@ def ukloni_vikend():
 
     subota.grid_remove()
     nedelja.grid_remove()
+
+
 
 def upisipredmet():
     dodaj.configure(bg=random.choice(colors))
@@ -104,29 +106,27 @@ def upisipredmet():
 
     j = vremepocetkacasa - 7
 
-    if vremepocetkacasa <= 7 or vremepocetkacasa + duzinatrajanjacasa > 21:
-        tkinter.messagebox.showinfo("Upozorenje",
-                                    "Greska!\nMolimo Vas da unesete tacno vreme i duzinu trajanja casa."
-                                    "\nCasovi pocinju u 8:00, a zavrsavaju se u 21:00.")
-    else:
+    if proveri_zauzeto(i - 6, j - 1, j - 1 + duzinatrajanjacasa):
 
-        if proveri_zauzeto(i - 6, j - 1, j - 2 + duzinatrajanjacasa):
-
-            if vremepocetkacasa >= 8 and vremepocetkacasa <= 21 and vremepocetkacasa + duzinatrajanjacasa <= 21:
-                while duzinatrajanjacasa > 0:
-                    if j <= 13:
-                        matrica[i - 6][j - 1] = predmet
-                        l = Label(text=predmet, relief=RIDGE, bg="lightblue").grid(row=i, column=j, sticky=NSEW)
-                        duzinatrajanjacasa = duzinatrajanjacasa - 1
-                        j = j + 1
-
-            else:
-                tkinter.messagebox.showinfo("Upozorenje",
-                                            "Greska!\nMolimo Vas da unesete tacno vreme i duzinu trajanja casa."
-                                            "\nCasovi pocinju u 8:00, a zavrsavaju se u 21:00.")
+        if vremepocetkacasa >= 8 and vremepocetkacasa <= 21 and vremepocetkacasa + duzinatrajanjacasa <= 21:
+            while duzinatrajanjacasa > 0:
+                if j <= 13:
+                    matrica[i - 6][j - 1] = predmet
+                    l = Label(text=predmet, relief=RIDGE, bg="lightblue").grid(row=i, column=j, sticky=NSEW)
+                    duzinatrajanjacasa = duzinatrajanjacasa - 1
+                    j = j + 1
 
         else:
-            tkinter.messagebox.showinfo("Upozorenje", "Ovaj termin je zauzet.\nMolimo Vas izaberite neki drugi termin!")
+            tkinter.messagebox.showinfo("Upozorenje",
+                                        "Greska!\nMolimo Vas da unesete tacno vreme i duzinu trajanja casa."
+                                        "\nCasovi pocinju u 8:00, a zavrsavaju se u 21:00.")
+
+    else:
+        tkinter.messagebox.showinfo("Upozorenje", "Ovaj termin je zauzet.\nMolimo Vas izaberite neki drugi termin!")
+    # for i in range(0, 7):
+    # for j in range(0, 14):
+    # print(matrica[i][j])
+    # print("\n")
 
 def obrisipredmet():
     obrisi.configure(bg=random.choice(colors))
@@ -169,75 +169,6 @@ def obrisipredmet():
     else:
         tkinter.messagebox.showinfo("Upozorenje", "Greska!\nMolimo Vas da unesete tacno vreme i duzinu trajanja casa."
                                                   "\nCasovi pocinju u 8:00, a zavrsavaju se u 21:00.")
-
-
-"""" Da li navedeni predmet postoji u rasporedu
-def predmet_info():
-    ime = proveri_predmet.get()
-
-    m = 8
-    n = 14
-    k = 15
-
-    if not ime == "":
-        for i in range(0, 7):
-            for j in range(0, 14):
-                if matrica[i][j] == ime:
-                    if i == 0:
-                        dan = "Ponedeljak"
-                    elif i == 1:
-                        dan = "Utorak"
-                    elif i == 2:
-                        dan = "Sreda"
-                    elif i == 3:
-                        dan = "Cetvrtak"
-                    elif i == 4:
-                        dan = "Petak"
-                    elif i == 5:
-                        dan = "Subota"
-                    else:
-                        dan = "Nedelja"
-
-                    if j == 0:
-                        vreme = "08:00 - 09:00"
-                    elif j == 1:
-                        vreme = "09:00 - 10:00"
-                    elif j == 2:
-                        vreme = "10:00 - 11:00"
-                    elif j == 3:
-                        vreme = "11:00 - 12:00"
-                    elif j == 4:
-                        vreme = "12:00 - 13:00"
-                    elif j == 5:
-                        vreme = "13:00 - 14:00"
-                    elif j == 6:
-                        vreme = "14:00 - 15:00"
-                    elif j == 7:
-                        vreme = "15:00 - 16:00"
-                    elif j == 8:
-                        vreme = "16:00 - 17:00"
-                    elif j == 9:
-                        vreme = "17:00 - 18:00"
-                    elif j == 10:
-                        vreme = "18:00 - 19:00"
-                    elif j == 11:
-                        vreme = "19:00 - 20:00"
-                    else:
-                        vreme = "20:00 - 21:00"
-
-                    lab1 = Label(text=dan, relief=RIDGE)
-                    lab1.grid(column=m, row=n)
-
-                    lab2 = Label(text=vreme, relief=RIDGE)
-                    lab2.grid(column=m, row=k)
-
-                    m=m+1
-
-
-    else:
-        tkinter.messagebox.showinfo("Greska!", "Molimo Vas unesite ime rasporeda!")
-
-"""
 
 
 window = Tk()
@@ -364,25 +295,13 @@ vikendm.grid(column=7, row=2, sticky=NSEW)
 
 wb = xlwt.Workbook()
 
-excel = Button(window, text="Save", command=exportuj)
-excel.grid(column=3, row=14, sticky=NSEW)
+excel = Button(window, text="Sacuvaj", command=exportuj)
+excel.grid(column=6, row=16, sticky=NSEW)
+
+iii = Label (window, text="Raspored: ")
+iii.grid(column=6, row= 15, sticky=NSEW)
 
 ime_excel = Entry(window, width=11)
-ime_excel.grid(column=4, row=14)
-
-# pokusaj open excel fajla
-# excel_open = Button(window, text="Open")
-# excel_open.grid(column=8, row=14, sticky=NSEW)
-
-# ime_excel_open = Entry(window, width=11)
-# ime_excel_open.grid(column=9, row=14)
-
-
-# proveri = Button(window, text="Proveri", command=predmet_info)
-# proveri.grid(column=6, row=14, sticky=NSEW)
-
-# proveri_predmet = Entry(window, width=11)
-# proveri_predmet.grid(column=7, row=14)
-
+ime_excel.grid(column=7, row=15)
 
 window.mainloop()
